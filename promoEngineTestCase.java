d2f4e3c51f241fbf6c5a5f161247d79fbd8c4d3b
poojasingh2021 <95176985+poojasingh2021@users.noreply.github.com>
2021-11-28 20:49:13 +0530

    Update promoEngineTestCase.java

--- promoEngineTestCase.java
+++ promoEngineTestCase.java
@@ -11,6 +11,7 @@ public class promoEngineTestCase {
 	        assertEquals(100,obj.calculateTotalPrice(new String[]{"A","B","C"}));  
 	        assertEquals(370,obj.calculateTotalPrice(new String[]{"A","A","A","A","A","B","B","B","B","B","C"}));  
 	        assertEquals(280,obj.calculateTotalPrice(new String[]{"A","A","A","B","B","B","B","B","C","D"}));  
-		assertEquals(80,obj.calculateTotalPrice(new String[]{"A","B","E"}));  
+		assertEquals(80,obj.calculateTotalPrice(new String[]{"A","B","E"})); 
+		assertEquals(0,obj.calculateTotalPrice(null)); 
 	    } 
 }
