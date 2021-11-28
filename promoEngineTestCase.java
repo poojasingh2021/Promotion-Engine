1cb0dc44f2cde854fb89f4197206905628ad8121
poojasingh2021 <95176985+poojasingh2021@users.noreply.github.com>
2021-11-28 20:45:17 +0530

    Update promoEngineTestCase.java

--- promoEngineTestCase.java
+++ promoEngineTestCase.java
@@ -7,9 +7,10 @@ import promotionEngine.PriceCalculator;
 public class promoEngineTestCase {
 	   @Test  
 	    public void testCalculateTotalPrice(){  
-		   PriceCalculator obj = new PriceCalculator();
+		PriceCalculator obj = new PriceCalculator();
 	        assertEquals(100,obj.calculateTotalPrice(new String[]{"A","B","C"}));  
 	        assertEquals(370,obj.calculateTotalPrice(new String[]{"A","A","A","A","A","B","B","B","B","B","C"}));  
 	        assertEquals(280,obj.calculateTotalPrice(new String[]{"A","A","A","B","B","B","B","B","C","D"}));  
+		assertEquals(80,obj.calculateTotalPrice(new String[]{"A","B","E"}));  
 	    } 
 }
